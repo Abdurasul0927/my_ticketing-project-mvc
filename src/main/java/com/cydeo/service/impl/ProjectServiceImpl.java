883adbf096432e1e3bcd25f2f5043385c763f5e5
rasulshirinboev <rasulboev9@gmail.com>
2022-12-13 09:25:59 -0500

    Ticketing Part-5

--- src/main/java/com/cydeo/service/impl/ProjectServiceImpl.java
+++ src/main/java/com/cydeo/service/impl/ProjectServiceImpl.java
@@ -1,9 +1,11 @@
 package com.cydeo.service.impl;
 
 import com.cydeo.dto.ProjectDTO;
+import com.cydeo.dto.TaskDTO;
 import com.cydeo.dto.UserDTO;
 import com.cydeo.enums.Status;
 import com.cydeo.service.ProjectService;
+import com.cydeo.service.TaskService;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -11,6 +13,12 @@ import java.util.stream.Collectors;
 
 @Service
 public class ProjectServiceImpl extends AbstractMapService<ProjectDTO,String>implements ProjectService {
+
+    private final TaskService taskService;
+
+    public ProjectServiceImpl(TaskService taskService) {
+        this.taskService = taskService;
+    }
 
     @Override
     public ProjectDTO save(ProjectDTO project) {
@@ -60,6 +68,8 @@ public class ProjectServiceImpl extends AbstractMapService<ProjectDTO,String>imp
                         .stream()
                         .filter(project -> project.getAssignedManager().equals(manager))
                         .map(project ->{
+
+                            List<TaskDTO> taskList = taskService.findTasksByManager(manager);
 
                             int completeTaskCounts = 5;
                             int unfinishedTaskCounts = 3;

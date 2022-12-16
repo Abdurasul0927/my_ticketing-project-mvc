f338cef5f5c620d20a47445b2a2138297623b533
rasulshirinboev <rasulboev9@gmail.com>
2022-12-16 13:24:45 -0500

    Validation

--- src/main/java/com/cydeo/dto/UserDTO.java
+++ src/main/java/com/cydeo/dto/UserDTO.java
@@ -28,6 +28,9 @@ public class UserDTO {
     @Pattern(regexp = "(?=.\\d)(?=.[a-z])(?=.*[A-Z]).{4,}")
     private String passWord;
 
+    @NotNull
+    private String confirmPassWord;
+
     private boolean enabled;
 
     @NotBlank
@@ -39,4 +42,86 @@ public class UserDTO {
 
    @NotNull
     private Gender gender;
+
+    public String getPassWord() {
+        return passWord;
+    }
+
+    public void setPassWord(String passWord) {
+        this.passWord = passWord;
+        checkConfirmPassword();
+    }
+
+    public String getConfirmPassWord() {
+        return confirmPassWord;
+    }
+
+    public void setConfirmPassWord(String confirmPassWord) {
+        this.confirmPassWord = confirmPassWord;
+        checkConfirmPassword();
+    }
+
+    private void checkConfirmPassword() {
+        if (this.passWord == null || this.confirmPassWord == null) {
+            return;
+        } else if (!this.passWord.equals(this.confirmPassWord)) {
+            this.confirmPassWord = null;
+        }
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public boolean isEnabled() {
+        return enabled;
+    }
+
+    public void setEnabled(boolean enabled) {
+        this.enabled = enabled;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public RoleDTO getRole() {
+        return role;
+    }
+
+    public void setRole(RoleDTO role) {
+        this.role = role;
+    }
+
+    public Gender getGender() {
+        return gender;
+    }
+
+    public void setGender(Gender gender) {
+        this.gender = gender;
+    }
 }
